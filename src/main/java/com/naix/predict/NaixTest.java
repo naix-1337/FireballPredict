5b5f0c78faa3b536d5cdac5b4805902290c5a5d9
naix1337 <264750094+naix-1337@users.noreply.github.com>
2026-07-12 21:15:13 +0800

    chore: 版本号改为 2.0

--- src/main/java/com/naix/predict/NaixTest.java
+++ src/main/java/com/naix/predict/NaixTest.java
@@ -31,7 +31,7 @@ import java.util.List;
 public class NaixTest
 {
     public static final String MODID = "fireball_predict";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "2.0";
 
     // 开关状态 (PredictionRenderer 读取)
     public static boolean enabled = true;
