2d27eb7542b42ed01e968b13fa84589a8bb3ca55
naix1337 <264750094+naix-1337@users.noreply.github.com>
2026-07-13 16:42:50 +0800

    chore: 版本号升至 2.2

--- src/main/java/com/naix/predict/FireballPredict.java
+++ src/main/java/com/naix/predict/FireballPredict.java
@@ -31,7 +31,7 @@ import java.util.List;
 public class FireballPredict
 {
     public static final String MODID = "fireball_predict";
-    public static final String VERSION = "2.1";
+    public static final String VERSION = "2.2";
 
     // 开关状态 (PredictionRenderer 读取)
     public static boolean enabled = true;
